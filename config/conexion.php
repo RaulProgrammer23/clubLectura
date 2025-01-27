730770532c07ef12e7df27316ec2629d8fdff963
Raul Programmer <rcavprogrammer@gmail.com>
2025-01-27 10:48:07 +0100

    first commit clubLectura

--- config/conexion.php
+++ config/conexion.php
@@ -4,7 +4,7 @@ class Conexion{
         try {
             //abrimos la conexión
             $opciones = array(PDO::MYSQL_ATTR_INIT_COMMAND=>"SET NAMES utf8mb4");
-            $bd = new PDO("mysql:host=localhost;dbname=club_lectores","raul","rm2223",$opciones); // root , rm2223
+            $bd = new PDO("mysql:host=localhost;dbname=club_lectores","","",$opciones); 
             $bd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $p) {
             echo "Error ".$p->getMessage()."<br />";
